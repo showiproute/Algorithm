612c62d0df3917701b17aee03e2abd0711be306f
wecallitabill <m18752095913@163.com>
2019-03-19 18:46:08 +0800

    Pat review

--- src/PAT/Java_14.java
+++ src/PAT/Java_14.java
@@ -1,6 +1,6 @@
 package PAT;
 
-import java.util.ArrayList;
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Java_14 {
@@ -9,34 +9,10 @@ public class Java_14 {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		String[] fields = scanner.nextLine().split(".");
-		
-		ArrayList<String> numlist = new ArrayList<>();
-		ArrayList<String> resultlist=new ArrayList<>();
-		
-		String flag = fields[0].split("")[0];
-		numlist.add(fields[0].split("")[1]);
-		numlist.add(fields[1].split("E")[0]);
-		String suffix = fields[1].split("E")[1];
-		String suffixflag = suffix.split("")[0];
-		String suffixnums = suffix.split("")[0];
-		
-		
-		
-		if(flag.equals("-")) {
-			resultlist.add("-");
-			if(suffixflag.equals("-")) {
-				
-			}
-		
-		}
-		
-		
-		
-		
-		
-		
-		
+
+		BigDecimal s=scanner.nextBigDecimal();
+		System.out.println(s.toPlainString());
+		scanner.close();
 		
 	}
 	
