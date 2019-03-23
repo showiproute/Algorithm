728dc7a4cceb42d53732206f35d1e29e7f11c91f
wecallitabill <m18752095913@163.com>
2019-03-23 15:10:13 +0800

    Offer Review

--- src/Offer/BinaryTree_2.java
+++ src/Offer/BinaryTree_2.java
@@ -1,5 +1,58 @@
 package Offer;
 
+/*
+ * 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
+ */
 public class BinaryTree_2 {
 
+	
+	public  static class TreeNode {
+	    int val = 0;
+	    TreeNode left = null;
+	    TreeNode right = null;
+
+	    public TreeNode(int val) {
+	        this.val = val;
+
+	    }
+
+	}
+	
+    public static boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        boolean result=false;
+        if(root1!=null && root2!=null) {
+        	if(root1.val==root2.val) {
+        		result=doesTree1inTree2(root1, root2);
+        	}
+        	
+        	if(!result) {
+        		result=doesTree1inTree2(root1.left,root2);
+        	}
+        	if(!result) {
+        		result=doesTree1inTree2(root1.right, root2);
+        	}
+        }	
+    	return result;
+    }
+    
+    
+    public static boolean doesTree1inTree2(TreeNode root1,TreeNode root2) {
+    	if(root2==null) {
+    		return true;
+    	}
+    	
+    	if(root1==null) {
+    		return false;
+    	}
+    	
+    	if(root1.val!=root2.val) {
+    		return false;
+    	}
+    	
+
+    	
+    	return doesTree1inTree2(root1.left, root2.left) && doesTree1inTree2(root1.right, root2.right);
+    }
+    
+    
 }
--- src/Offer/BinaryTree_3.java
+++ src/Offer/BinaryTree_3.java
@@ -1,5 +1,63 @@
 package Offer;
 
-public class BinaryTree_3 {
+import java.util.Stack;
 
+/*
+ * 操作给定的二叉树，将其变换为源二叉树的镜像。
+ * 二叉树的镜像定义：源二叉树 
+    	    8
+    	   /  \
+    	  6   10
+    	 / \  / \
+    	5  7 9 11
+    	镜像二叉树
+    	    8
+    	   /  \
+    	  10   6
+    	 / \  / \
+    	11 9 7  5
+ */
+public class BinaryTree_3 {
+	
+	public static class TreeNode {
+	    int val = 0;
+	    TreeNode left = null;
+	    TreeNode right = null;
+
+	    public TreeNode(int val) {
+	        this.val = val;
+
+	    }
+
+	}
+	
+    public static void Mirror(TreeNode root) {
+     
+    	if(root==null) {
+    		return;
+    	}
+    	
+    	Stack<TreeNode> stack = new Stack<>();    	
+    	stack.push(root);
+    	while(!stack.isEmpty()) {
+    		TreeNode node = stack.pop();
+    		     
+    		if(node.left!=null || node.right !=null) {
+    			TreeNode temp = node.left;
+    			node.left=node.right;
+    			node.right=temp;
+    		
+    		}
+    		
+    		if(node.left!=null) {
+    			stack.push(node.left);
+    		}
+    		
+    		if(node.right!=null) {
+    			stack.push(node.right);
+    		}
+    		
+    	}
+    	
+    }
 }
