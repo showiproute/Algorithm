a9804830ed944b721d74da89969d0082895fcca1
wecallitabill <m18752095913@163.com>
2019-05-02 23:38:37 +0800

    HuaweiReview Coding

--- src/HuaweiCoding/String_15.java
+++ src/HuaweiCoding/String_15.java
@@ -1,18 +1,49 @@
 package HuaweiCoding;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.Scanner;
-/*
- * ddd
- */
+
 public class String_15 {
 
+	public static String stringTrans(String inputLine) {
+		char[] charArray = inputLine.toCharArray();
+		LinkedList<Character> linkedList=new LinkedList<>();
+		
+		for(int i=0;i<charArray.length;i++) {
+			if(Character.isLetter(charArray[i])) {
+				linkedList.add(charArray[i]);
+			}
+		}
+		Collections.sort(linkedList,new Comparator<Character>() {
+
+			@Override
+			public int compare(Character o1, Character o2) {
+				// TODO Auto-generated method stub
+				return Character.toLowerCase(o1)-Character.toLowerCase(o2);
+			}
+		});
+		StringBuilder sb=new StringBuilder();
+		int counts=0;
+		for(int i=0;i<charArray.length;i++) {
+			if(Character.isLetter(charArray[i]) && counts<linkedList.size()) {
+				sb.append(linkedList.get(counts));
+				counts++;
+			}else {
+				sb.append(charArray[i]);
+			}
+		}
+		
+		return sb.toString();
+	}
 	
 	
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		while(sc.hasNext()) {
 			String inputLine = sc.nextLine();
-			
+			System.out.println(stringTrans(inputLine));
 		}
 		
 	}
