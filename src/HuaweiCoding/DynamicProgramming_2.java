adbc170905cb7c26432e9f824f0ec81102759c03
wecallitabill <m18752095913@163.com>
2019-05-02 23:26:44 +0800

    HuaWeiReview

---
+++ src/HuaweiCoding/DynamicProgramming_2.java
@@ -0,0 +1,25 @@
+package HuaweiCoding;
+
+import java.util.Scanner;
+
+public class DynamicProgramming_2 {
+
+	public static void standOut(int[] inputStu) {
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		while(sc.hasNext()) {
+			int num = sc.nextInt();
+			int[] stu=new int[num];
+			for(int i=0;i<num;i++) {
+				stu[i]=sc.nextInt();
+			}
+		}
+		
+		
+	}
+	
+}
---
+++ src/HuaweiCoding/String_15.java
@@ -0,0 +1,20 @@
+package HuaweiCoding;
+
+import java.util.Scanner;
+/*
+ * ddd
+ */
+public class String_15 {
+
+	
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		while(sc.hasNext()) {
+			String inputLine = sc.nextLine();
+			
+		}
+		
+	}
+	
+}
