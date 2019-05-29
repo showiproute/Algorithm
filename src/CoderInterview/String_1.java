c1f660ddbc46d589e27b9f5780a0b8f04c952be6
wecallitabill <m18752095913@163.com>
2019-05-29 20:10:47 +0800

    CoderInterview

--- src/CoderInterview/String_1.java
+++ src/CoderInterview/String_1.java
@@ -1,39 +1,21 @@
 package CoderInterview;
 
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.Map.Entry;
+import java.util.*;
 
 public class String_1 {
 
-	public static boolean isSame(String inputLine) {
+	public  boolean checkDifferent(String inputLine) {
 		
-		HashMap<Character, Integer> hashMap=new HashMap<>();
+		boolean[] flag=new boolean[65536];
 		for(int i=0;i<inputLine.length();i++) {
 			char item = inputLine.charAt(i);
-			if(hashMap.containsKey(item)) {
-				hashMap.put(item, hashMap.get(item)+1);
-			}else {
-				hashMap.put(item, 1);
-			}
-		}
-		
-		for(Entry<Character,Integer> keyValue:hashMap.entrySet()) {
-			if(keyValue.getValue()!=1) {
+			if(flag[item]) {
 				return false;
 			}
+			flag[item]=true;
 		}
-
+		
 		return true;
-	}
-	
-	
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		while(sc.hasNext()) {
-			String inputLine = sc.nextLine();
-			System.out.println(isSame(inputLine));
-		}
 		
 	}
 	
---
+++ src/CoderInterview/String_2.java
@@ -0,0 +1,18 @@
+package CoderInterview;
+
+import java.util.*;
+public class String_2 {
+
+    public String reverseString(String iniString) {
+        // write code here
+    	char[] charArray = iniString.toCharArray();
+    	for(int i=0;i<iniString.length()/2;i++) {
+    		char temp=charArray[charArray.length-1-i];
+    		charArray[charArray.length-i-1]=charArray[i];
+    		charArray[i]=temp;
+    	}
+    	
+    	return new String(charArray);
+    }
+	
+}
